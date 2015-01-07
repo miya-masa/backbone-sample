aeeed621504a88837c8d83496c88ae49c229e193
miya-masa <miyauchi0623@gmail.com>
2015-01-07 11:27:26 +0900

    remove files

--- backbone-todo/app/scripts/todo-view.js
+++
@@ -1,8 +0,0 @@
-var Backbone = require('backbone');
-module.exports = Backbone.View.extend({
-    template: _.template('<tr><td>1</td><td>task</td><td>‚µ‚ß‚«‚è</td><td><button type="button" class="btn btn-danger">íœ</button></td></tr>'),
-    render: function() {
-        this.$el.html(this.template(this.model.attributes));
-        return this;
-    }
-});
